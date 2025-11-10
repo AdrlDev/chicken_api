5ae5e7d6d07477317532f1a39ec9d421353a9cc1
adrielespiritu <adriel.dev.123@gmail.com>
2025-11-10 23:30:03 +0800

    aaa

--- app/train_model.py
+++ app/train_model.py
@@ -8,7 +8,6 @@ import subprocess
 from datetime import datetime
 from ultralytics import YOLO  # type: ignore
 from app.utils import BASE_DIR, DATASET_DIR
-from ultralytics.yolo.engine.model import YOLO as YOLOModel
 
 # Thread lock for safe YOLO reloading
 reload_lock = threading.Lock()
@@ -135,7 +134,7 @@ def train_yolo_autosplit(dataset_dir: str,
             model = YOLO(start_weights)
         else:
             # Train fresh: initialize a new model directly on uploaded images
-            model = YOLOModel(data_yaml_path)  # empty YOLO model
+            model = YOLO(data_yaml_path)  # empty YOLO model
         model.to(device)
         model.train(
             data=data_yaml_path,
