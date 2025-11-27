67cdac835ff26171eb3b4e1d7147150dd3a20f7a
adrielespiritu <adriel.dev.123@gmail.com>
2025-11-27 22:26:15 +0800

    okay

--- app/auth/security.py
+++ app/auth/security.py
@@ -17,7 +17,7 @@ def create_access_token(data: dict) -> str:
     
     # Calculate expiration time
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
+    to_encode.update({"exp": expire.timestamp()})
     
     # Encode the JWT using the secret key and algorithm
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
